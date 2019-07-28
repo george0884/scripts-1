75fa22abb77a1de16abb53697c72a03c6bb79ae0
brookiestein <brookie1@protonmail.com>
2019-07-28 14:47:29 -0400

    Save the signature in plain text.

--- endecrypt/crypt.sh
+++ endecrypt/crypt.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-# Copyright (C) 19aa Lord Brookie
-# Este programa es software libre. Puede redistribuirlo y/o
-# modificarlo bajo los términos de la Licencia Pública General
-# de GNU según es publicada por la Free Software Foundation,
-# bien de la versión 2 de dicha Licencia o bien --según su
-# elección-- de cualquier versión posterior.
-# Este programa se distribuye con la esperanza de que sea
-# útil, pero SIN NINGUNA GARANTÍA, incluso sin la garantía
-# MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN
-# PROPÓSITO PARTICULAR. Para más detalles, véase la Licencia
-# Pública General de GNU.
-# Debería haber recibido una copia de la Licencia Pública
-# General junto con este programa. En caso contrario, escriba
-# a la Free Software Foundation, Inc., en 675 Mass Ave,
-# Cambridge, MA 02139, EEUU.
 
 function help()
 {
@@ -113,7 +98,7 @@ if [ $? -eq 0 ]; then
                                 echo; echo "Suma de comprobación: md5 creada con éxito."
                                 echo "Creando firma GPG..."
                                 sleep 1
-                                gpg -o $name_final.sig -abq $name_final
+                                gpg -o $name_final.sig -abq --clearsign $name_final
                                 if [ $? -eq 0 ]; then
                                         echo; echo "Todos los trabajos han sido finalizados con éxito."; echo
                                 else
